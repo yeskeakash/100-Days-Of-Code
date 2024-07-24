f045d1f2f2d4ea5a05e3a30a68d834846f1df891
yeskeakash <yeskeakash@gmail.com>
2024-07-24 11:14:28 +0530

    push using LL

---
+++ 02_Day/stack_LL.cpp
@@ -0,0 +1,55 @@
+/*
+creating stack using Linked  Lists and performing operations on them 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) : data(x), next(nullptr){}
+};
+
+class Stack{
+  private:
+    Node* top;
+  
+  public:
+    Stack() : top(nullptr) {}
+
+    //push using LL
+  void push(int x){
+    Node *t = new Node(x);
+    if(t==NULL){
+      cout<<"stack overflow\n";
+      return;
+    }
+    t->data = x;
+    t->next = top;
+    top = t;
+  }
+
+  void Display(){
+    struct Node *p;
+    p=top;
+    while(p!=nullptr){
+      cout<<p->data<<" ";
+      p = p->next;
+    }
+    cout<<endl;
+  }
+};
+
+int main(){
+  Stack st;
+  st.push(10);
+  st.push(11);
+  st.push(12);
+  st.push(13);
+  st.push(14);
+  st.Display();
+}
